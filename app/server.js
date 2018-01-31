ff6bf4c0ebf70d909286031dae2fa07eac5e4fc0
erictenenbaum <erictenenbaum10@gmail.com>
2018-01-31 11:05:35 -0700

    inital commit

---
+++ app/server.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const path = require('path');
+
+var app = express();
+var PORT = 3000;
+
+var port = process.env.PORT || 3000;
+
+
